f7d84dcb28fef3c56467fed6d9be9f68cef73262
Ole Schütt <ole.schuett@cp2k.org>
2019-11-04 12:12:00 +0100

    Add more node pools with multiple K80 GPUs

--- setup/create_node_pools.sh
+++ setup/create_node_pools.sh
@@ -37,10 +37,12 @@ for CPUS in 8 16 32 ; do
         --node-taints="costly=true:NoSchedule"
 done
 
-gcloud beta container node-pools create pool-highcpu-16-haswell-tesla-k80 \
+for GPUS in 1 2 4 8 ; do
+    CPUS=$((8 * ${GPUS}))
+    gcloud beta container node-pools create pool-highcpu-${CPUS}-haswell-tesla-k80 \
        --cluster="${CLUSTER_NAME}" \
-       --machine-type="n1-highcpu-16" \
-       --accelerator="type=nvidia-tesla-k80,count=2" \
+       --machine-type="n1-highcpu-${CPUS}" \
+       --accelerator="type=nvidia-tesla-k80,count=${GPUS}" \
        --min-cpu-platform="Intel Haswell" \
        --preemptible \
        --enable-autoupgrade \
@@ -50,20 +52,7 @@ gcloud beta container node-pools create pool-highcpu-16-haswell-tesla-k80 \
        --min-nodes=0 \
        --num-nodes=0 \
        --node-taints="costly=true:NoSchedule"
-
-gcloud beta container node-pools create pool-highcpu-8-haswell-tesla-k80 \
-       --cluster="${CLUSTER_NAME}" \
-       --machine-type="n1-highcpu-8" \
-       --accelerator="type=nvidia-tesla-k80,count=1" \
-       --min-cpu-platform="Intel Haswell" \
-       --preemptible \
-       --enable-autoupgrade \
-       --enable-autorepair \
-       --enable-autoscaling \
-       --max-nodes=1 \
-       --min-nodes=0 \
-       --num-nodes=0 \
-       --node-taints="costly=true:NoSchedule"
+done
 
 # There is no n1-highcpu-24 machine type.
 # Using custom type with same 0.9GB/vCPU ratio routed up to multiple of 256MiB.
