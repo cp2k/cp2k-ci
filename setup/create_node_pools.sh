9277af7edce16472508b0a3f2d28cef272b79e41
Ole Schütt <ole.schuett@cp2k.org>
2021-03-01 11:08:37 +0100

    Switch GPU node pools to Skylake, remove K80

--- setup/create_node_pools.sh
+++ setup/create_node_pools.sh
@@ -24,30 +24,13 @@ for CPUS in 8 16 32 64 ; do
         --node-taints="costly=true:NoSchedule"
 done
 
-for GPUS in 2 4 ; do
-    CPUS=$((8 * ${GPUS}))
-    gcloud beta container node-pools create pool-tesla-k80-haswell-${CPUS} \
-       --cluster="${CLUSTER_NAME}" \
-       --machine-type="n1-standard-${CPUS}" \
-       --accelerator="type=nvidia-tesla-k80,count=${GPUS}" \
-       --min-cpu-platform="Intel Haswell" \
-       --preemptible \
-       --enable-autoupgrade \
-       --enable-autorepair \
-       --enable-autoscaling \
-       --max-nodes=1 \
-       --min-nodes=0 \
-       --num-nodes=0 \
-       --node-taints="costly=true:NoSchedule"
-done
-
 # There is no n1-standard-24 machine type.
 # Using custom type with same 3.75GB/vCPU ratio.
-gcloud container node-pools create pool-tesla-p4-haswell-24 \
+gcloud container node-pools create pool-tesla-p4-skylake-24 \
        --cluster="${CLUSTER_NAME}" \
        --machine-type="custom-24-92160" \
        --accelerator="type=nvidia-tesla-p4,count=1" \
-       --min-cpu-platform="Intel Haswell" \
+       --min-cpu-platform="Intel Skylake" \
        --preemptible \
        --enable-autoupgrade \
        --enable-autorepair \
@@ -57,11 +40,11 @@ gcloud container node-pools create pool-tesla-p4-haswell-24 \
        --num-nodes=0 \
        --node-taints="costly=true:NoSchedule"
 
-gcloud container node-pools create pool-tesla-p100-haswell-16 \
+gcloud container node-pools create pool-tesla-p100-skylake-16 \
        --cluster="${CLUSTER_NAME}" \
        --machine-type="n1-standard-16" \
        --accelerator="type=nvidia-tesla-p100,count=1" \
-       --min-cpu-platform="Intel Haswell" \
+       --min-cpu-platform="Intel Skylake" \
        --preemptible \
        --enable-autoupgrade \
        --enable-autorepair \
