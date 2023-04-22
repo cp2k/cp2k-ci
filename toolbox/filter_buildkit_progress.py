a02c0ce9ac02d1d377e99a51f3e69f18137b67c7
Ole Schuett <ole.schuett@cp2k.org>
2023-04-22 15:48:46 +0200

    Filter output from docker image pushing

--- toolbox/filter_buildkit_progress.py
+++ toolbox/filter_buildkit_progress.py
@@ -10,7 +10,9 @@ timing_pattern = re.compile(r"\d+(\.\d*)?")
 for line in sys.stdin:
     parts = line.split(" ")
     if len(parts) > 1 and timing_pattern.fullmatch(parts[1]):
-        sys.stdout.write(" ".join(parts[2:]))
+        sys.stdout.write(" ".join(parts[2:]))  # remove timing
+    elif len(parts) > 2 and parts[1] == "pushing" and parts[2] == "layer":
+        continue  # remove entire line
     else:
         sys.stdout.write(line)
     sys.stdout.flush()
